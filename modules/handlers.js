d81c333b194b4190f2b37496220a886aa5891e48
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-16 13:57:29 +0200

    Add show()

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,16 +1,24 @@
 var fs = require('fs');
+var formidable = require('formidable');
+var mv = require('move-file');
 
 exports.upload = function(request, response) {
   console.log('upload');
-  response.write('Upload started!');
-  response.end();
-}
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(error, fields, files) {
+    mv(files.upload.path, './test.png');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Received file:<br/>');
+    response.write('<img src="/show" />');
+    response.end();
+  });
+ }
 
 exports.welcome = function(request, response) {
   console.log('home');
   fs.readFile('templates/start.html', function(err, file) {
     if (err) throw err;
-    response.writeHead(200, { 'Content-Type': 'text-plain; charset=utf-8'});
+    response.writeHead(200, {'Content-Type': 'text-plain; charset=utf-8'});
     response.write(file);
     response.end();
   });
@@ -20,4 +28,13 @@ exports.error = function(request, response) {
   console.log('error');
   response.write('404');
   response.end();
+}
+
+exports.show = function(request, response) {
+  fs.readFile('test.png', 'binary', function(err, file) {
+    if (err) throw err;
+    response.writeHead(200, {'Content-Type': 'image/png'});
+    response.write(file, 'binary');
+    response.end();
+  })
 }
